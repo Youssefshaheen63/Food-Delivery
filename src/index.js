f534d6dca1f7274164f0a9888f73fc6273ab2e30
SalmaYasserMoselhi <salmayasser816@gmail.com>
2024-04-29 01:01:44 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
       <App />
-    </RecoilRoot>
   </React.StrictMode>
 );
