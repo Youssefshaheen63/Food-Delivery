e087ace0704723ef8ebb4731706c84aae7626ecf
RanaMohamed24 <raanaamohamed695@gmail.com>
2024-05-03 22:31:16 +0300

    checkout css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Footer from "./components/Footer";
 import Header from "./components/header";
+import Checkout from "./pages/checkout";
 import Routing from "./routing/routing";
 
 const App = () => {
   return (
     <>
-      <Header></Header>
-      <Routing></Routing>
+       <Header></Header> 
+       <Routing></Routing>
+     
+     
     </>
   );
 };
